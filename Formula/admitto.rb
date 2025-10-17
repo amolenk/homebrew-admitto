b0abaa9c7a1df14a550c0d308f9862bc2b110396
Sander Molenkamp <sander@sandermolenkamp.com>
2025-10-17 17:08:07 +0200

    Add formula

---
+++ Formula/admitto.rb
@@ -0,0 +1,23 @@
+class Admitto < Formula
+  desc "Admitto CLI"
+  homepage "https://github.com/amolenk/Admitto"
+  url "PUT_URL_OF_TARBALL_HERE"
+  sha256 "PUT_SHA256_OF_TARBALL_HERE"
+  version "PUT_VERSION_NUMBER_HERE"
+  license "MIT"
+
+  # Requires .NET SDK to build
+  depends_on "dotnet@9"
+
+  # Publish app as framework-dependent (requires user to have .NET runtime)
+  # Include the tarball sha256 in the informational version.
+  system "dotnet", "publish", "-c", "Release", "-o", "out",
+        "-p:AssemblyVersion=#{version}", "-p:FileVersion=#{version}", "-p:InformationalVersion=#{version}-#{sha256}"
+
+  # Install the executable into Homebrew’s bin dir
+  bin.install "out/admitto"
+
+  test do
+    system "#{bin}/admitto", "--version"
+  end
+end
