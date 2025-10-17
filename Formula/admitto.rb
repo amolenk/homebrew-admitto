23291022acd144456a1f2e639298a245285b00f0
Sander Molenkamp <sander@sandermolenkamp.com>
2025-10-17 18:21:11 +0200

    Fix formula

--- Formula/admitto.rb
+++ Formula/admitto.rb
@@ -4,19 +4,20 @@ class Admitto < Formula
   url "https://github.com/amolenk/Admitto/archive/3051b9eab88cce25c10ced998d1d3b54accd60f1.tar.gz"
   sha256 "c2cfaff710566a74c63c4836ca7186edc6474f461e49f8454510743543a2bc9f"
   version "1.0.12"
-  commit "3051b9eab88cce25c10ced998d1d3b54accd60f1"
   license "MIT"
 
   # Requires .NET SDK to build
   depends_on "dotnet@9"
 
-  # Publish app as framework-dependent (requires user to have .NET runtime)
-  # Include the tarball sha256 in the informational version.
-  system "dotnet", "publish", "-c", "Release", "-o", "out",
-        "-p:AssemblyVersion=#{version}", "-p:FileVersion=#{version}", "-p:InformationalVersion=#{version}-#{commit}"
+  def install
+    # Publish app as framework-dependent (requires user to have .NET runtime)
+    # Include the tarball sha256 in the informational version.
+    system "dotnet", "publish", "-c", "Release", "-o", "out",
+           "-p:AssemblyVersion=#{version}", "-p:FileVersion=#{version}", "-p:InformationalVersion=#{version}"
 
-  # Install the executable into Homebrew’s bin dir
-  bin.install "out/admitto"
+    # Install the executable into Homebrew’s bin dir
+    bin.install "out/admitto"
+  end
 
   test do
     system "#{bin}/admitto", "--version"
